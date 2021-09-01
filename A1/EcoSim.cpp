53d137401ac71a3e61c9dd767b5a0a2acbf401ba
Jordy <Jordy.larrearodriguez@gmail.com>
2021-09-01 12:07:22 -0600

    Created the A1 project and cpp file.

---
+++ A1/EcoSim.cpp
@@ -0,0 +1,8 @@
+/*
+* CS 3505 Assignment 1: An Ecological Simulation
+* Description:
+*   A console app that generates plots of rabbit and fox population curves.
+*
+* Author: Jordy A. Larrea Rodriguez (U1236145)
+*/
+#include <iostream>
