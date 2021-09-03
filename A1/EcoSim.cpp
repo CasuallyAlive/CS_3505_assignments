8edf5ad8dc5cf84d612ac611d9bb5f9dc8d5de06
Jordy <Jordy.larrearodriguez@gmail.com>
2021-09-02 22:40:38 -0600

    Population Simulator App: V 1.0: Implemented functionality to the extent of course specifications. Needs to be rigorously tested.

--- A1/EcoSim.cpp
+++ A1/EcoSim.cpp
@@ -6,9 +6,10 @@
 * Author: Jordy A. Larrea Rodriguez (U1236145)
 */
 #include <iostream>
+#include <string>
 
 /*
-* This function should set the parameters needed for the update equation as given above,
+* This function should set the parameters needed for the update equation,
 * and run the simulation for iterations steps or until the predator or prey population goes below 1.
 */
 void runSimulation(int, double, double);
@@ -30,8 +31,80 @@ void plotPopulations(double, double, double);
 */
 void incrementCounter(int *);
 
+const int iterations = 500;
+const double scaleFactor = 0.1;
+
 int main()
 {
+    double foxPopulation, rabbitPopulation;
+    std::cout << "This is a fox and rabbit population simulator." << std::endl;
+    std::cout << "Please input the initial fox population: ";
+    std::cin >> foxPopulation;
+    std::cout << "and the initial rabbit population: ";
+    std::cin >> rabbitPopulation;
 
-    return 0;
+    runSimulation(iterations, rabbitPopulation, foxPopulation);
+}
+
+void runSimulation(int iterations, double rabbitPopulation, double foxPopulation)
+{
+    double g{0.2}, p{0.0022}, c{0.6}, m{0.2}, K{1000.0};
+    for (int i = 0; i < iterations && (rabbitPopulation > 1 && foxPopulation > 1); incrementCounter(&i))
+    {
+        plotPopulations(rabbitPopulation, foxPopulation, scaleFactor);
+        std::cout << std::endl;
+        updatePopulations(g, p, c, m, K, rabbitPopulation, foxPopulation);
+    }
+}
+
+void updatePopulations(double g, double p, double c, double m, double K, double &numRabbits, double &numFoxes)
+{
+    double deltaRabbitPopulation = g * numRabbits * (1 - (numRabbits / K)) - p * numRabbits * numFoxes;
+    double deltaFoxPopulation = c * p * numRabbits * numFoxes - m * numFoxes;
+
+    numRabbits += deltaRabbitPopulation;
+    numFoxes += deltaFoxPopulation;
+}
+
+void plotCharacter(int numberOfSpaces, char letter)
+{
+    std::string row = "";
+    for (int i = 0; i <= numberOfSpaces; i++)
+    {
+        row = (i == numberOfSpaces) ? row + letter : row + " ";
+    }
+    std::cout << row;
+}
+
+void plotPopulations(double rabbitPopulation, double foxPopulation, double scaleFactor)
+{
+    int rabbitCharPosition, foxCharPosition;
+    if (rabbitPopulation == foxPopulation)
+    {
+        int position = floor(scaleFactor * rabbitPopulation);
+        plotCharacter(position, '*');
+        return;
+    }
+    if (rabbitPopulation < foxPopulation)
+    {
+        rabbitCharPosition = floor(scaleFactor * rabbitPopulation);
+        // plots the char for rabbits at it's corresponding location.
+        plotCharacter(rabbitCharPosition, 'r');
+        foxCharPosition = floor(scaleFactor * foxPopulation) - rabbitPopulation;
+        // plots the char for foxes at it's corresponding location.
+        plotCharacter(foxCharPosition, 'F');
+        return;
+    }
+
+    foxCharPosition = floor(scaleFactor * rabbitPopulation);
+    // plots the char for rabbits at it's corresponding location.
+    plotCharacter(foxCharPosition, 'F');
+    rabbitCharPosition = floor(scaleFactor * foxPopulation) - rabbitPopulation;
+    // plots the char for foxes at it's corresponding location.
+    plotCharacter(rabbitCharPosition, 'r');
+}
+
+void incrementCounter(int *count)
+{
+    *count++;
 }
