b4097158a0e88eaa1a17b1124edc2b03f25c113c
Jordy <Jordy.larrearodriguez@gmail.com>
2021-09-02 23:38:21 -0600

    Eco Simulator App: V 1.1: Removed program breaking bugs. Simulation now runs as expected. To-do: tidy up the code.

--- A1/EcoSim.cpp
+++ A1/EcoSim.cpp
@@ -7,6 +7,7 @@
 */
 #include <iostream>
 #include <string>
+#include <cmath>
 
 /*
 * This function should set the parameters needed for the update equation,
@@ -31,19 +32,37 @@ void plotPopulations(double, double, double);
 */
 void incrementCounter(int *);
 
+/*
+* Prints the row of the chart.
+*/
+void printRow();
+
 const int iterations = 500;
 const double scaleFactor = 0.1;
+std::string row = "";
 
 int main()
 {
     double foxPopulation, rabbitPopulation;
     std::cout << "This is a fox and rabbit population simulator." << std::endl;
     std::cout << "Please input the initial fox population: ";
-    std::cin >> foxPopulation;
+
+    if (!(std::cin >> foxPopulation))
+    {
+        std::cout << "The input is not a valid, please restart the program." << std::endl;
+        return -1;
+    }
     std::cout << "and the initial rabbit population: ";
-    std::cin >> rabbitPopulation;
+    if (!(std::cin >> rabbitPopulation))
+    {
+        std::cout << "The input is not a valid, please restart the program." << std::endl;
+        return -1;
+    }
 
     runSimulation(iterations, rabbitPopulation, foxPopulation);
+    std::cout << "The simulation has terminated, press 'Enter' to exit." << std::endl;
+    std::cin.ignore();
+    std::cin.get();
 }
 
 void runSimulation(int iterations, double rabbitPopulation, double foxPopulation)
@@ -52,7 +71,6 @@ void runSimulation(int iterations, double rabbitPopulation, double foxPopulation
     for (int i = 0; i < iterations && (rabbitPopulation > 1 && foxPopulation > 1); incrementCounter(&i))
     {
         plotPopulations(rabbitPopulation, foxPopulation, scaleFactor);
-        std::cout << std::endl;
         updatePopulations(g, p, c, m, K, rabbitPopulation, foxPopulation);
     }
 }
@@ -68,43 +86,50 @@ void updatePopulations(double g, double p, double c, double m, double K, double 
 
 void plotCharacter(int numberOfSpaces, char letter)
 {
-    std::string row = "";
     for (int i = 0; i <= numberOfSpaces; i++)
     {
         row = (i == numberOfSpaces) ? row + letter : row + " ";
     }
-    std::cout << row;
 }
 
 void plotPopulations(double rabbitPopulation, double foxPopulation, double scaleFactor)
 {
     int rabbitCharPosition, foxCharPosition;
-    if (rabbitPopulation == foxPopulation)
+    if (std::floor(rabbitPopulation) == std::floor(foxPopulation))
     {
-        int position = floor(scaleFactor * rabbitPopulation);
+        int position = std::floor(scaleFactor * rabbitPopulation);
         plotCharacter(position, '*');
+        printRow();
         return;
     }
     if (rabbitPopulation < foxPopulation)
     {
-        rabbitCharPosition = floor(scaleFactor * rabbitPopulation);
+        rabbitCharPosition = std::floor(scaleFactor * rabbitPopulation);
         // plots the char for rabbits at it's corresponding location.
         plotCharacter(rabbitCharPosition, 'r');
-        foxCharPosition = floor(scaleFactor * foxPopulation) - rabbitPopulation;
+        foxCharPosition = std::floor(scaleFactor * foxPopulation) - rabbitCharPosition;
         // plots the char for foxes at it's corresponding location.
         plotCharacter(foxCharPosition, 'F');
+        printRow();
         return;
     }
 
-    foxCharPosition = floor(scaleFactor * rabbitPopulation);
+    foxCharPosition = std::floor(scaleFactor * foxPopulation);
     // plots the char for rabbits at it's corresponding location.
     plotCharacter(foxCharPosition, 'F');
-    rabbitCharPosition = floor(scaleFactor * foxPopulation) - rabbitPopulation;
+    rabbitCharPosition = std::floor(scaleFactor * rabbitPopulation) - foxCharPosition;
     // plots the char for foxes at it's corresponding location.
     plotCharacter(rabbitCharPosition, 'r');
+    printRow();
+}
+
+void printRow()
+{
+    std::cout << row << std::endl;
+    row = "";
 }
 
 void incrementCounter(int *count)
 {
-    *count++;
+    (*count)++;
 }
